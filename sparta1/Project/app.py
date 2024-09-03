8ba4d18e152c43c0dc5f3b170157d2bdbaa92c9f
su2708 <46125751+su2708@users.noreply.github.com>
2024-09-03 14:43:43 +0900

    [Melody share] - show a favortie song

--- sparta1/Project/app.py
+++ sparta1/Project/app.py
@@ -3,8 +3,32 @@
 0. Flask : 웹서버를 시작할 수 있는 기능. app이라는 이름으로 플라스크를 시작한다
 1. render_template : html파일을 가져와서 보여준다
 '''
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
+
+# DB 기본 코드
+import os
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database.db')
+
+db = SQLAlchemy(app)
+
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    artist = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    image_url = db.Column(db.String(10000), nullable=False)
+
+    def __repr__(self):
+        return f'{self.title} {self.artist} 추천 by {self.username}'
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def home():
@@ -30,6 +54,21 @@ def iloveyou(name):
         'motto': motto,
     }
     return render_template('motto.html', data=context)
+
+@app.route("/music/create/")
+def music_create():
+    # form에서 보낸 데이터 받아오기
+    username_receive = request.args.get("username")
+    title_receive = request.args.get("title")
+    artist_receive = request.args.get("artist")
+    image_receive = request.args.get("image_url")
+    
+    # 데이터를 DB에 저장하기
+    song = Song(username=username_receive, title=title_receive, artist=artist_receive, image_url=image_receive)
+    db.session.add(song)
+    db.session.commit()
+    
+    return render_template('music.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
