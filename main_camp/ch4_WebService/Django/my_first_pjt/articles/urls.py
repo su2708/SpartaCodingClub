d6474bf1f35e0d78384342d724f3234eb839b9ff
su2708 <46125751+su2708@users.noreply.github.com>
2024-12-18 22:42:16 +0900

    Update: Django MTV 사용하기 (CR)

--- main_camp/ch4_WebService/Django/my_first_pjt/articles/urls.py
+++ main_camp/ch4_WebService/Django/my_first_pjt/articles/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("hello/", views.hello, name="hello"),
+    path("", views.articles, name="articles"),
+    path("new/", views.new, name="new"),
+    path("create/", views.create, name="create"),
+    #
+    #
+    path("index/", views.index, name="index"),
     # data
     path("data-throw/", views.data_throw, name="data-throw"),
     path("data-catch/", views.data_catch, name="data-catch"),
--- main_camp/ch4_WebService/Django/my_first_pjt/articles/views.py
+++ main_camp/ch4_WebService/Django/my_first_pjt/articles/views.py
@@ -1,25 +1,32 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
 
-def hello(request):
-    name = "Yun"
-    tags = ["#joyful", "#bright", "#cute"]
-    books = {
-        "today": "토지",
-        "yesterday": "태백산맥",
-        "tomorrow": "우정",
-    }
-    
+def articles(request):
+    articles = Article.objects.all().order_by("-id")
     context = {
-        "name": name,
-        "tags": tags,
-        "books": books,
+        "articles": articles,
     }
-    
-    return render(request, "hello.html", context)
+    return render(request, "articles.html", context)
+
+def new(request):
+    return render(request, "new.html")
+
+def create(request):
+    # GET 방식으로 전달된 데이터를 받아서
+    title = request.POST.get("title")
+    content = request.POST.get("content")
+
+    # 받은 데이터를 새로운 Article 모델을 이용해서 저장
+    article = Article(title=title, content=content)
+    article.save()
+    context = {
+        "article": article,
+    }
+    return render(request, "create.html", context)
 
 def data_throw(request):
     return render(request, "data-throw.html")
