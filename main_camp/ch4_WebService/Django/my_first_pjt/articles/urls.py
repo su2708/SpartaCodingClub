234b17da61b8661c7a0b0351e8878dead00837d4
su2708 <46125751+su2708@users.noreply.github.com>
2024-12-19 00:57:52 +0900

    Update: Django MTV 사용하기 (RUD)

--- main_camp/ch4_WebService/Django/my_first_pjt/articles/urls.py
+++ main_camp/ch4_WebService/Django/my_first_pjt/articles/urls.py
@@ -3,9 +3,12 @@ from . import views
 
 urlpatterns = [
     path("", views.articles, name="articles"),
-    path("new/", views.new, name="new"),
-    path("create/", views.create, name="create"),
-    #
+    path("new/", views.new, name="new"),  # 새로운 article 작성 
+    path("create/", views.create, name="create"),  # 새로운 article 생성
+    path("<int:pk>/", views.article_detail, name="article_detail"),  # article 상세 
+    path("<int:pk>/delete/", views.delete, name="delete"),  # article 삭제 
+    path("<int:pk>/edit/", views.edit, name="edit"),  # article 수정 사항 작성 
+    path("<int:pk>/update/", views.update, name="update"),  # article 수정 사항 반영 
     #
     path("index/", views.index, name="index"),
     # data
--- main_camp/ch4_WebService/Django/my_first_pjt/articles/views.py
+++ main_camp/ch4_WebService/Django/my_first_pjt/articles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
 
 # Create your views here.
@@ -6,11 +6,24 @@ def index(request):
     return render(request, "index.html")
 
 def articles(request):
+    # Article에 저장된 걸 다 가져와 id 내림차순으로 정렬 
     articles = Article.objects.all().order_by("-id")
+    
     context = {
         "articles": articles,
     }
+    
     return render(request, "articles.html", context)
+
+def article_detail(request, pk):
+    # Article에 저장된 목록 중에 pk==pk 인 글 가져오기 
+    article = Article.objects.get(pk=pk)
+    
+    context = {
+        "article": article,
+    }
+    
+    return render(request, "article_detail.html", context)
 
 def new(request):
     return render(request, "new.html")
@@ -21,12 +34,35 @@ def create(request):
     content = request.POST.get("content")
 
     # 받은 데이터를 새로운 Article 모델을 이용해서 저장
-    article = Article(title=title, content=content)
+    article = Article.objects.create(title=title, content=content)
+    
+    # 작업이 다 끝나면 articles.html로 redirect 
+    return redirect('article_detail', article.pk)
+
+def edit(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {"article": article} 
+    return render(request, "edit.html", context)
+
+def update(request, pk):
+    title = request.POST.get("title")
+    content = request.POST.get("content")
+    
+    article = Article.objects.get(pk=pk)
+    article.title = title
+    article.content = content
     article.save()
-    context = {
-        "article": article,
-    }
-    return render(request, "create.html", context)
+    
+    return redirect("article_detail", article.pk)
+
+def delete(request, pk):
+    # method == "POST" 인 경우에만 article 삭제 
+    if request.method == "POST":
+        # Article에 저장된 목록 중에 pk==pk 인 글 가져오기 
+        article = Article.objects.get(pk=pk)
+        article.delete()
+        return redirect("articles")
+    return redirect("article_detail", pk)
 
 def data_throw(request):
     return render(request, "data-throw.html")
@@ -37,4 +73,3 @@ def data_catch(request):
         "message": message,
     }
     return render(request, "data-catch.html", context)
-
