50df85195a84825d12f69e5b3027e52ae356885a
su2708 <46125751+su2708@users.noreply.github.com>
2024-12-21 14:04:37 +0900

    Update models.py

--- main_camp/ch4_WebService/Django/my_first_pjt/articles/models.py
+++ main_camp/ch4_WebService/Django/my_first_pjt/articles/models.py
@@ -14,3 +14,12 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    content = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content
