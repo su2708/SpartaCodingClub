09c7012b9dc804fc206b876f0712b14036ecaba4
su2708 <46125751+su2708@users.noreply.github.com>
2024-12-29 03:02:28 +0900

    Update serializers.py

--- main_camp/ch4_WebService/DRF/articles/serializers.py
+++ main_camp/ch4_WebService/DRF/articles/serializers.py
@@ -16,6 +16,8 @@ class CommentSerializer(serializers.ModelSerializer):
 
 class ArticleSerializer(serializers.ModelSerializer):
     comments = CommentSerializer(many=True, read_only=True)
+    # comments_count는 직접 필드를 추가해 주는 것(Django가 알아서 추가해주는 것이 아님!)
+    # source 속성을 이용하여 데이터 값을 전달
     comments_count = serializers.IntegerField(source="comments.count", read_only=True)
     
     class Meta:
