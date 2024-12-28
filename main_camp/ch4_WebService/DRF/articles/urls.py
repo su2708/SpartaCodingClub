6cd7fe271e41cdb92dcccc2520bae8d3fd23a4a1
su2708 <46125751+su2708@users.noreply.github.com>
2024-12-28 20:41:36 +0900

    Update:Relationship과 DRF

--- main_camp/ch4_WebService/DRF/articles/urls.py
+++ main_camp/ch4_WebService/DRF/articles/urls.py
@@ -6,4 +6,14 @@ app_name = "articles"
 urlpatterns = [    
     path("", views.ArticleListAPIView.as_view(), name="article_list_create"),
     path("<int:pk>/", views.ArticleDetailAPIView.as_view(), name="article_detail"),
+    path(
+        "<int:article_pk>/comments/",
+        views.CommentListAPIView.as_view(),
+        name="comment_list"
+    ),
+    path(
+        "comments/<int:comment_pk>/",
+        views.CommentDetailAPIView.as_view(),
+        name="comment_detail"
+    ),
 ]
--- main_camp/ch4_WebService/DRF/articles/views.py
+++ main_camp/ch4_WebService/DRF/articles/views.py
@@ -4,9 +4,9 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import ArticleSerializer
+from .serializers import ArticleSerializer, ArticleDetailSerializer, CommentSerializer
 from django.shortcuts import render, get_object_or_404
-from .models import Article
+from .models import Article, Comment
 
 class ArticleListAPIView(APIView):
     def get(self, request):
@@ -24,18 +24,19 @@ class ArticleListAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+
 class ArticleDetailAPIView(APIView):
     def get_object(self, pk):
         return get_object_or_404(Article, pk=pk)
 
     def get(self, request, pk):
         article = self.get_object(pk)
-        serializer = ArticleSerializer(article)
+        serializer = ArticleDetailSerializer(article)
         return Response(serializer.data)
 
     def put(self, request, pk):
         article = self.get_object(pk)
-        serializer = ArticleSerializer(article, data=request.data, partial=True)
+        serializer = ArticleDetailSerializer(article, data=request.data, partial=True)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data)
@@ -45,3 +46,36 @@ class ArticleDetailAPIView(APIView):
         article.delete()
         data = {"pk": f"{pk} is deleted."}
         return Response(data, status=status.HTTP_200_OK)
+
+
+class CommentListAPIView(APIView):
+    def get(self, request, article_pk):
+        article = get_object_or_404(Article, pk=article_pk)
+        comments = article.comments.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request, article_pk):
+        article = get_object_or_404(Article, pk=article_pk)
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(article=article)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class CommentDetailAPIView(APIView):
+    def get_object(self, comment_pk):
+        return get_object_or_404(Comment, pk=comment_pk)
+    
+    def delete(self, request, comment_pk):
+        comment = self.get_object(comment_pk)
+        comment.delete()
+        data = {"pk": f"{comment_pk} is deleted."}
+        return Response(data, status=status.HTTP_200_OK)
+    
+    def put(self, request, comment_pk):
+        comment = self.get_object(comment_pk)
+        serializer = CommentSerializer(comment, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
