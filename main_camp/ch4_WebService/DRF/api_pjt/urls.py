850b197fa0aee30c6476d2bdf9557666b964e575
su2708 <46125751+su2708@users.noreply.github.com>
2025-01-01 02:23:47 +0900

    Update: 더 빠른 속도를 위해

--- main_camp/ch4_WebService/DRF/api_pjt/urls.py
+++ main_camp/ch4_WebService/DRF/api_pjt/urls.py
@@ -21,7 +21,8 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/articles/", include("articles.urls")),
-    path("api/v1/accounts/", include("accounts.urls"))
+    path("api/v1/accounts/", include("accounts.urls")),
+    path("api/v1/products/", include("products.urls")),
 ]
 
 urlpatterns += [path('silk/', include('silk.urls', namespace='silk'))]
--- main_camp/ch4_WebService/DRF/products/views.py
+++ main_camp/ch4_WebService/DRF/products/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.core.cache import cache
+from .models import Product
+from .serializers import ProductSerializer
+
 
 # Create your views here.
+@api_view(["GET"])
+def product_list(request):
+    cache_key = "product_list"  # redis caching 사용 
+    
+    if not cache.get(cache_key):
+        print("cache miss")
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        json_data = serializer.data
+        cache.set(cache_key, json_data, 180)  # cache_key, cache_value, expired_time
+    
+    response_data = cache.get(cache_key)
+    return Response(response_data)
